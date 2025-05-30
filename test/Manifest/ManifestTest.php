eef2f7a2648ae3341f1d07efbc14c720cb84785d
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-05-30 16:42:41 -0400

    Added testing for Manifest

---
+++ test/Manifest/ManifestTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LmcTest\Vite\Manifest;
+
+use Lmc\Vite\Manifest\Manifest;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+class ManifestTest extends TestCase
+{
+    public function testCreateDev(): void
+    {
+        $this->expectNotToPerformAssertions();
+        $manifest = new Manifest(
+            true,
+            __DIR__ . '/../Assets/manifest.json',
+            '/dist/'
+        );
+    }
+
+    public function testCreateNotDev(): void
+    {
+        $this->expectNotToPerformAssertions();
+        $manifest = new Manifest(
+            false,
+            __DIR__ . '/../Assets/manifest.json',
+            '/dist/'
+        );
+    }
+
+    public function testCreateNonReadableManifestFile(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $manifest = new Manifest(
+            false,
+            __DIR__ . '/../Assets/manifest-not-readable.json',
+            '/dist/'
+        );
+    }
+}
